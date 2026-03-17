1395273932c15c4b88b55860043c00479c04417f
Frederick Boehm <fboehm@protonmail.com>
2026-03-17 16:44:54 -0500

    fix: up build.sh to extract quarto tarball outside

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Install Quarto
-wget -qO quarto.tar.gz \
+# Install Quarto (extract outside the repo to avoid being picked up by quarto render)
+wget -qO /tmp/quarto.tar.gz \
   "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz"
-tar -xf quarto.tar.gz
-export PATH="$(pwd)/quarto-${QUARTO_VERSION}/bin:$PATH"
+tar -xf /tmp/quarto.tar.gz -C /tmp
+export PATH="/tmp/quarto-${QUARTO_VERSION}/bin:$PATH"
 
 # Render site
 quarto render
